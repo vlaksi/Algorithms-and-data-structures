52cefa8349409a0bb2216679ac9e4cf813e17e1b
vlaksi <daksrapsody@gmail.com>
2020-03-08 20:13:08 +0100

    binary tree postorder traversal with recursion

---
+++ algorithms-and-data-structures/trees-and-graphs/trees/binary-tree-postorder-traversal/solution-recursion.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def postorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        output = []
+        self.search(root,output)
+        return output
+        
+    def search(self, node, output):
+        if node is not None:
+            if node.left is not None:
+                self.search(node.left, output)
+            if node.right is not None:
+                self.search(node.right, output)
+            output.append(node.val)
