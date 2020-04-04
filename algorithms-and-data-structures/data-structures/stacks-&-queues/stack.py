0731b05c105f5147ce67929cf3f019d887e5d4a9
vlaksi <daksrapsody@gmail.com>
2020-04-04 18:56:14 +0200

    [data structure] - stack

---
+++ algorithms-and-data-structures/data-structures/stacks-&-queues/stack.py
@@ -0,0 +1,36 @@
+"""
+    Stack data struture. LIFO ]
+    We use python list to create Stack.
+"""
+
+
+class Stack():
+    def __init__(self):
+        self.items = []
+
+    # override of toString method
+    def __str__(self):
+        ret = "\n------- STACK -------\n"
+        # we go from back of list and print elements
+        for element in reversed(self.items):
+            ret += "\t\t" + str(element) +"\n"
+        ret += "---------------------"
+        return ret
+
+    # add element to top of stack
+    def push(self,item):
+        self.items.append(item)
+
+    # remove element from top of stack
+    def pop(self):
+        return self.items.pop()
+
+    def isEmpty(self):
+        return self.items == []
+
+    def peek(self):
+        if not self.isEmpty():
+            return self.items[-1]
+
+    def getStack(self):
+        return self.items
---
+++ algorithms-and-data-structures/data-structures/stacks-&-queues/test-stack.py
@@ -0,0 +1,20 @@
+
+"""
+    Test for our implementation of stack.
+"""
+
+from stack import Stack
+
+if __name__ == "__main__":
+
+    ourStack = Stack()
+    ourStack.push("A")
+    ourStack.push("B")
+    ourStack.push("C")
+    print(ourStack)
+
+    # after remove one element from top
+    # last element which is added now is removed
+    ourStack.pop()
+    print(ourStack)
+
